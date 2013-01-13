62f2ab43a7d10c6913f4fcb47eb0a019bbb0a38c
Kent Bye <kent@kentbye.com>
2013-01-13 12:16:02 -0800

    Added select list, changed to HTML5 document

--- currenttime.js
+++ currenttime.js
@@ -11,6 +11,15 @@ $ns.currenttime = function () {
 
     document.getElementById("$const.date").innerHTML = day + "." + month + "." + year + " " + hours+":"+minutes+":"+seconds;
 
+    // Populate the Current Date into Dropdowns
+    var yearselect = document.getElementById("year"), year;
+    var yeargenerate = function(max){
+          do{
+            yearselect.add(new Option(year++, max--), null);}
+            while(max>0);
+          }(5);
+
+    // Calculate the current positions of the planets
     $e.calculatedTime ();
 
 };
