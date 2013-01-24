77b4ef21f8b24ad83c3f2d87c96e723228c7518f
Kent Bye <kent@kentbye.com>
2013-01-24 15:02:26 -0800

    added arrow animation controls. Abstracted out aspect lines. Standardized colors.

--- calculatedTime.js
+++ calculatedTime.js
@@ -131,11 +131,11 @@ $ns.animateStop = function ()  {
 } 
 
 function createCookie(name,value,days) {
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime()+(days*24*60*60*1000));
-      var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/;";
+	if (days) {
+	  var date = new Date();
+	  date.setTime(date.getTime()+(days*24*60*60*1000));
+	  var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/;";
 }
